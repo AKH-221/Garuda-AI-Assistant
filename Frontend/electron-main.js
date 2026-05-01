c2849fdc974b0d2e95e0406583db530b357f2185
Anish Kedia <anishkedia2010@gmail.com>
2026-05-02 01:18:48 +0400

    chamges

---
+++ Frontend/electron-main.js
@@ -0,0 +1,28 @@
+import { app, BrowserWindow } from 'electron';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1000,
+    minHeight: 700,
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  // this loads your React app
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
